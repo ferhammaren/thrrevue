e6afc21412d89c8dde6b5c8c365d6087c50229e1
Fernanda Meza <fernanda.meza.lopez@gmail.com>
2025-06-03 22:57:13 -0700

    add malice mizer

---
+++ thrrevue/src/hooks/useIsVisible.js
@@ -0,0 +1,20 @@
+// src/hooks/useIsVisible.js
+import { useEffect, useState } from "react";
+
+export function useIsVisible(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIntersecting(entry.isIntersecting);
+    });
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
